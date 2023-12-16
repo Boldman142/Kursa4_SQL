06214ce445ba4bca2a6cdfb78663c83f2e35821f
Artem Blodov <mr.samy@rambler.ru>
2023-12-16 15:41:26 +0200

    add get_data, create_db

--- main.py
+++ main.py
@@ -4,21 +4,22 @@ from utils import get_hh_data, create_database, save_data_to_database
 
 def main():
     employer_ids = [
-        1740,  # Яндекс
-        2146335,  # 2гис
+        # 1740,  # Яндекс
+        64174,  # 2гис
         39305,  # газпром
-        78638,  # тинькофф
-        1473866,  # Сбер сервис
-        2211644,  # Росатом
-        3543069,  # Минэнерго
-        2624085,  # Вкусно и точка
-        23427,  # РЖД
-        1373  # Аэрофлот
+        # 78638,  # тинькофф
+        # 1473866,  # Сбер сервис
+        # 2211644,  # Росатом
+        # 3543069,  # Минэнерго
+        # 2624085,  # Вкусно и точка
+        # 23427,  # РЖД
+        # 1373  # Аэрофлот
     ]
     params = config()
     data = get_hh_data(employer_ids)
-    create_database('HeadHunt', params)
-    save_data_to_database(data, 'HeadHunt', params)
+    print(data)
+    # create_database('HeadHunt', params)
+    # save_data_to_database(data, 'HeadHunt', params)
 
 
 if __name__ == "__main__":
--- utils.py
+++ utils.py
@@ -1,26 +1,87 @@
 from typing import Any
 
+import psycopg2
 import requests
 
 
 def get_hh_data(ids: list):
     """Получает все вакансии от всех работодателей,
     список которых передается"""
-
-    params = {
-        'employer_id': 39305,  # ID 2ГИС
-        # 'area': area,         # Поиск в зоне
-        # 'page': page,         # Номер страницы
-        # 'per_page': 100  # Кол-во вакансий на 1 странице
-    }
-    req = requests.get('https://api.hh.ru/vacancies', params)
-    data = req.content.decode()
-    req.close()
+    data = []
+    for employer_id in ids:
+        page = 0
+        vacancies = []
+        req_emp = requests.get(f'https://api.hh.ru/employers/{employer_id}').json()
+        # print(req_emp.json())
+        employer_data = {
+            'id': req_emp['id'],
+            'name': req_emp['name'],
+            'site': req_emp.get('site_url'),
+            'url_hh': req_emp['alternate_url']
+        }
+        while True:
+            params = {
+                'employer_id': employer_id,  # id работодателя
+                'page': page,  # Номер страницы
+                'per_page': 50  # Кол-во вакансий на 1 странице
+            }
+            req = requests.get('https://api.hh.ru/vacancies', params)
+            # print(req.json())
+            vacancies.extend(req.json()['items'])
+            page += 1
+            if page == 2:  # req.json()['pages']:
+                req.close()
+                data.append({
+                    'employer_data': employer_data,
+                    'vacancies': vacancies
+                })
+                break
     return data
 
 
 def create_database(database_name: str, params: dict):
     """Создает базу данных с указанным именем и параметрами для конекта"""
+    conn = psycopg2.connect(dbname='postgres', **params)
+    conn.autocommit = True
+    cur = conn.cursor()
+
+    cur.execute(f"DROP DATABASE {database_name}")
+    cur.execute(f"CREATE DATABASE {database_name}")
+
+    conn.close()
+
+    conn = psycopg2.connect(dbname=database_name, **params)
+
+    with conn.cursor() as cur:
+        cur.execute("""
+            CREATE TABLE employers (
+                employer_id SERIAL PRIMARY KEY,
+                title VARCHAR(255) NOT NULL,
+                employer_is_hh INTEGER NOT NULL,
+                HH_url TEXT NOT NULL,
+                site_employer TEXT
+            )
+        """)
+
+    with conn.cursor() as cur:
+        cur.execute("""
+            CREATE TABLE vacancies (
+                vacancy_id SERIAL PRIMARY KEY,
+                employer_id INT REFERENCES employers(employer_id),
+                title VARCHAR NOT NULL,
+                salary_from INT,
+                salary_to INT,
+                city VARCHAR(50) NOT NULL,
+                url_vacancies TEXT NOT NULL,
+                requirements TEXT,
+                employment VARCHAR(50),
+                experience VARCHAR(50),
+                publish_date DATE                
+            )
+        """)
+
+    conn.commit()
+    conn.close()
 
 
 def save_data_to_database(data: list[dict[str, Any]], database_name: str, params: dict):
